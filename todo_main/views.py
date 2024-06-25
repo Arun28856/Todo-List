cfeef3d7d33983a11dd98b8bd4c59c54b0299902
Lakshman Arun P <arunlakshman28@gmail.com>
2024-06-26 00:29:29 +0530

    add task functionality

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,7 +3,11 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_complete=False).order_by('-updated_at')
+
+    completed_task = Task.objects.filter(is_complete=True)
+    print(completed_task)
     context = {
         'tasks': tasks,
+        'completed_task': completed_task,
     }
     return render(request, 'home.html', context)
